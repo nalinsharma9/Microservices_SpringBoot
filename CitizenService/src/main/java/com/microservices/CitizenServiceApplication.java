2ff75f06f87ae9b687f3165f97b0310fb66071c2
I563718 <nalin.sharma@sap.com>
2024-08-26 17:41:29 +0530

    First Commit

---
+++ CitizenService/src/main/java/com/microservices/CitizenServiceApplication.java
@@ -0,0 +1,15 @@
+package com.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitizenServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CitizenServiceApplication.class, args);
+    }
+
+}
+
+
